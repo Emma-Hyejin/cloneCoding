3ea7777e69935887f2fb3021c289620c87431510
emma <jhj191058@gmail.com>
2023-01-14 15:49:20 +0900

    feat : JS 2차 시도 [실패]
    getElementsByClassName  적용

--- codePen/loginPage_01/script.js
+++ codePen/loginPage_01/script.js
@@ -1,26 +1,4 @@
-(function(){
-    "use strict"
-
-    const change = document.getElementsByClassName("sign-form");
-    const original = document.getElementsByClassName("login-form");
-
-    document.getElementById("login").onclick = function(){
-        if(change.style.display == 'none'){
-            change.style.display = 'visible';
-            original.style.display = 'none';
-        }
-    };
-
-    document.getElementById("create").onclick = function(){
-        if(original.style.display == 'none' ){
-            original.style.display = 'visible';
-            change.style.display = 'none';
-        }
-    }
-
-});
-
-
-// $(changeclick).click(function(){
-//     $('.form .sign-form:visible').animate({height:"toggle", opacity:"toggle"}, "slow");
-// }); 
+function test1(){
+    document.getElementsByClassName("login-form")[0].style.display = 'none'; 
+    document.getElementsByClassName("sign-form")[0].style.display = 'visible';
+}
